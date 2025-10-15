cf58d49c438777f39cdeda1bd78301d423064514
Ross Daly <rdaly525@gmail.com>
2025-10-15 09:38:02 -0700

    Adds an SSA-based printer

--- src/puzzlespec/puzzles/__init__.py
+++ src/puzzlespec/puzzles/__init__.py
@@ -1,9 +1,10 @@
+from puzzlespec.compiler.dsl.spec_builder import PuzzleSpecBuilder
 from .unruly import build_unruly_spec
 from .sudoku import build_sudoku_spec
 import sys
 from ..compiler.dsl.spec import PuzzleSpec
 
-def get_puzzle(name: str) -> PuzzleSpec:
+def get_puzzle(name: str) -> PuzzleSpecBuilder:
     build_name = f"build_{name}_spec"
     if not hasattr(sys.modules[__name__], build_name):
         raise ValueError(f"Puzzle {name} not found")
